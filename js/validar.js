2a678de80973822d4da3c2f52073125df864efe8
Mar Mazuelas <mm.zlsmng@gmail.com>
2019-05-15 12:44:16 +0200

    correccion de errores

--- js/validar.js
+++ js/validar.js
@@ -1,19 +1,26 @@
 //Programado por: Mar Mazuelas - Fecha:15/05/2019
-function validar() {
-    $(document).ready(function () {
+$(document).ready(function () {
+    $("#codigopostal").on("change keyup paste", function () {
         let patron = /^[0-9]{5}$/;
-        let cp = $(codigopostal).val();
+        let cp = $("#codigopostal").val();
 
         if (patron.test(cp)) {
-            setTimeout(function () {
-                //"El código postal introducido es correcto.";
-                $(#codigopostal).html("El código postal introducido es correcto.");
-            }, 3000);
+            if (cp.length === 5) { //"El código postal introducido es correcto.";
+                $("#mostrar").html("El código postal introducido es correcto.");
+                borrar();
+            }
         } else {
-            setTimeout(function () {
+            if (cp !== "") {
                 //"El codigo postal no es correcto. Debe ser de 5 caracteres."
-                $(#codigopostal).html("El código postal no es correcto. Debe introducir 5 caracteres numéricos.");
-            }, 3000);
+                $("#mostrar").html("El código postal no es correcto. Debe introducir 5 caracteres numéricos.");
+                borrar();
+            }
         }
     });
+});
+
+function borrar() {
+    setTimeout(function () {
+        $("#mostrar").html("");
+    }, 3000);
 }
